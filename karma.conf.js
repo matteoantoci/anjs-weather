86ab8391b4e021791fa3417f4d386fab072c166e
mantoci <mantoci@ebay.com>
2014-10-12 18:08:36 +0200

    Cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -5,10 +5,9 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/**/*.js'
     ],
 
     autoWatch : true,
